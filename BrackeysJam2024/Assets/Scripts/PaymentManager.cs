df676b17ffb1486fcdd6092f834e6e3f6466c8d7
Cody Beinert <codybeinert123@gmail.com>
2024-09-10 10:51:34 -0400

    make a (currently kind of broken) upgrade/payment system

---
+++ BrackeysJam2024/Assets/Scripts/PaymentManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaymentManager : MonoBehaviour
+{
+    public int cost;
+}
--- BrackeysJam2024/Assets/Scripts/Upgrading.cs
+++ BrackeysJam2024/Assets/Scripts/Upgrading.cs
@@ -8,6 +8,8 @@ public class Upgrading : MonoBehaviour
 
     public HealthBar healthBar;
 
+    int payment;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,16 +23,38 @@ public class Upgrading : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                this.GetComponent<PlayerHealth>().maxHealth += (this.GetComponent<PlayerHealth>().maxHealth * 0.1f);
+                Debug.Log("increase health");
+                this.GetComponent<PlayerHealth>().maxHealth += 10;
+                this.GetComponent<PlayerHealth>().currentHealth = this.GetComponent<PlayerHealth>().maxHealth;
+                healthBar.SetMaxHealth(this.GetComponent<PlayerHealth>().maxHealth);
+                this.GetComponent<CoinCollection>().coins -= payment;
             }
         }
     }
 
-    private void OnCollisionStay(Collision collision)
+
+    private void OnTriggerStay(Collider other)
     {
-        if (collision.gameObject.tag == "HealthUpgrade")
+        if (other.gameObject.tag == "HealthUpgrade")
         {
-            upgradeHealth = true;
+            if (other.gameObject.GetComponent<PaymentManager>() != null && this.GetComponent<CoinCollection>().coins >= other.gameObject.GetComponent<PaymentManager>().cost)
+            {
+                upgradeHealth = true;
+                payment = other.gameObject.GetComponent<PaymentManager>().cost;
+            }
+            else if (this.GetComponent<CoinCollection>().coins < other.gameObject.GetComponent<PaymentManager>().cost)
+            {
+                upgradeHealth = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "HealthUpgrade")
+        {
+            upgradeHealth = false;
+            payment = 0;
         }
     }
 
