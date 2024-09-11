c2c73a8cc3fa161e3dcd3c94b6ad394566cbdac1
Cody Beinert <codybeinert123@gmail.com>
2024-09-11 16:08:16 -0400

    add text UI for cost of  upgrades

--- BrackeysJam2024/Assets/Scripts/PaymentManager.cs
+++ BrackeysJam2024/Assets/Scripts/PaymentManager.cs
@@ -5,4 +5,21 @@ using UnityEngine;
 public class PaymentManager : MonoBehaviour
 {
     public int cost;
+    public bool canShow;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canShow = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canShow = false;
+        }
+    }
 }
