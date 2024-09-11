d191d397d9cb28cecf26f183be361349fffa0b73
Cody Beinert <codybeinert123@gmail.com>
2024-09-11 17:46:40 -0400

    fix error with enemy bullet spawning (sort of)

--- BrackeysJam2024/Assets/Scripts/SpawnBullet.cs
+++ BrackeysJam2024/Assets/Scripts/SpawnBullet.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class SpawnBullet : MonoBehaviour
 {
     public GameObject bullet;
+    public float time;
+    public float startTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawningBullet", 0, 2);
+        //InvokeRepeating("SpawningBullet", 0, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
+        time -= 1 * Time.deltaTime;
+        if (time < 0)
+        {
+            time = startTime;
+        }
 
+        SpawningBullet();
     }
 
     public void SpawningBullet()
     {
-        Instantiate(bullet, this.transform.position, Quaternion.identity);
-        Debug.Log("Bullet has spawned");
+        if (time <= 0.01)
+        {
+            Instantiate(bullet, this.transform.position, Quaternion.identity);
+            Debug.Log("Bullet has spawned");
+        }
     }
 }
